af921300455e41705d619db737f04a9adf3509e1
GHhuang1057 <huang1057@outlook.com>
2025-05-05 15:47:25 +0800

    emmmm

---
+++ Batbbf.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace BBKS6玩机工具箱
+{
+    public partial class Batbbf : Window
+    {
+        public Batbbf()
+        {
+            InitializeComponent();
+        }
+    }
+}
